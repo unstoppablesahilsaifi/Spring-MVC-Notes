ace6c3c0d761fe44f8bf91a12bd8a1f0c2aa8b3e
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2023-01-04 15:10:12 +0530

    Create 1_Configuration.java

---
+++ 1_Configuration.java
@@ -0,0 +1,7 @@
+1. If .jsp file gives error:
+Right click on projects -> properties -> Java Build Path -> remove existing JRE file and Add latest library in library section.
+then add tomcat library also -> add library -> server runtime -> Apache tomcat. Apply.
+//Error resolve
+
+
+    
